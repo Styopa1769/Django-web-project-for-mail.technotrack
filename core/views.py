8e2a187fb0c9b3b08fc9b1034162543bd3f0bbe8
Shcherbakov Stepan <shc-stepan@yandex.ru>
2018-03-13 01:09:00 +0300

    all work

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+
+    name = request.GET.get('name')
+    return HttpResponse('Hello {}'.format(name))
