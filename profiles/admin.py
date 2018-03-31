838970a9214f3eb3ac45de2e95f0cc5f193455f7
Shcherbakov Stepan <shc-stepan@yandex.ru>
2018-04-01 02:11:41 +0300

    31.03.2018

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+@admin.register(Blog)
+class Blog(admin.ModelAdmin):
+
+    list_display = 'name', 'author'
+    search_fields = 'name', 'author__username'
+    list_filter = 'is_archive',
--- profiles/models.py
+++ profiles/models.py
@@ -10,6 +10,20 @@ from events.models import Event
 
 class Blog(models.Model):
 
-    author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    name = models.CharField(max_length=255)
-    events = models.ManyToManyField(Event, blank=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='blogs',
+        verbose_name=u'Автор'
+    )
+    name = models.CharField(max_length=255, verbose_name=u'Имя блога')
+    is_archive=models.BooleanField(default=False, verbose_name=u'Блог в архиве')
+    #events = models.ManyToManyField(Event, blank=True, related_name='blogs')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = u'Блог'
+        verbose_name_plural = u'Блоги'
+        ordering = 'name', 'id'
+
+    def __unicode__(self):
+        return self.name
